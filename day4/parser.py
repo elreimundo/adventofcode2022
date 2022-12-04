a291944cde823546203763ccc5877715fa03dd97
David Reiman <david.reiman@gmail.com>
2022-12-04 09:59:02 -0800

    A bit of cleanup

--- day4/parser.py
+++ day4/parser.py
@@ -1,12 +1,11 @@
 from my_range import Range
 
 def parse(input: str) -> (Range, Range):
-	[[left_min, left_max], [right_min, right_max]] = [
-		[
+	return [
+		Range(*[
 			int(value) for value in split_at(tuple, "-")
-		] for tuple in split_at(input, ",")
+		]) for tuple in split_at(input, ",")
 	]
-	return (Range(left_min, left_max), Range(right_min, right_max))
 
 def split_at(input: str, on: str) -> (str, str):
 	return [part.strip() for part in input.split(on)]
